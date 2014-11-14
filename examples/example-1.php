da6977f3f967e82dbfa4e8c7e9a53759d71a78c7
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-11-14 16:11:16 +0000

    Upload initial version of library, mostly ported from Moodle

---
+++ examples/example-1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Reading Lists API for is-dev applications.
+ *
+ * @package    ReadingLists
+ * @copyright  2014 Skylar Kelty <S.Kelty@kent.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . "/../src/API.php");
+require_once(dirname(__FILE__) . "/../src/List.php");
+require_once(dirname(__FILE__) . "/../src/Parser.php");
+
+$api = new \ReadingLists\API();
+
+$lists = $api->get_lists("EN902");
+
+foreach ($lists as $list) {
+    echo "---------------------------------\n";
+    echo $list;
+}
