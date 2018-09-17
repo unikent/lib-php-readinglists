2e7c4baf0388f167e5060c2a3187e9f866344a54
Dan Bartlett <d.bartlett@kent.ac.uk>
2018-09-17 14:36:28 +0100

    Quote out Ticket functions for get_categories so only for reading lsit test

--- examples/example-2/run.php
+++ examples/example-2/run.php
@@ -11,23 +11,24 @@ require_once("vendor/autoload.php");
 
 $api = new \unikent\ReadingLists\API();
 $api->set_timeout(4000);
-$api->set_timeperiod("2014");
+$api->set_timeperiod("2018");
 $api->set_campus("canterbury");
 
 $lists = $api->get_lists("LW509");
-
+/*
 function print_cats($level, $category) {
     echo str_repeat('-', $level) . " " . $category->get_name() . "\n";
     foreach ($category->get_parents() as $cat) {
         print_cats($level + 1, $cat);
     }
 }
-
+*/
 foreach ($lists as $list) {
     echo "---------------------------------\n";
-    echo $list;
-
+    echo $list."\n";
+/*
     foreach ($list->get_categories() as $category) {
         print_cats(1, $category);
     }
+*/
 }
